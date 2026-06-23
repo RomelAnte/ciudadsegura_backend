8dcda5d29495752d41e23a01b623300db880e373
RomelAnte <rsag20092002@gmail.com>
2026-06-22 22:22:41 -0500

    first commit

---
+++ Aplications/reportes/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Report(models.Model):
+    id = models.AutoField(primary_key=True)
+    description = models.TextField()
+    address = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    picture = models.ImageField(upload_to='report_pictures/', null=True, blank=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.address
